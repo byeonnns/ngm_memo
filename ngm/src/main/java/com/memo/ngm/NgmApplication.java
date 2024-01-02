a9faa92a55be9f45533c01980ab1842c736ebc6d
byeonnns <tjdwns3823@naver.com>
2024-01-03 01:00:15 +0900

    first commit

---
+++ ngm/src/main/java/com/memo/ngm/NgmApplication.java
@@ -0,0 +1,13 @@
+package com.memo.ngm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NgmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NgmApplication.class, args);
+	}
+
+}
---
+++ ngm/src/test/java/com/memo/ngm/NgmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.memo.ngm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NgmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
